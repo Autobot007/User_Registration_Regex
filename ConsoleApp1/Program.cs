433e3255b7b43113d29d80a3491fa6e3bee8beae
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-09-17 10:47:24 +0530

    User Registration Program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace User_Regestration_Regex
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome User Registration with Regex");
+            Console.WriteLine("====================================");
+        }
+    }
+}
